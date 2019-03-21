5786c7f3e751863e0933569481f37f717ff98995
Scott Bennett <stt.btt@gmail.com>
2019-03-21 10:50:26 -0400

    MVP

--- ex6/ex6.c
+++ ex6/ex6.c
@@ -20,20 +20,45 @@ and `clock_gettime()` should work just fine.
 
 int main()
 {
-    printf("Starting from (pid: %d)\n", (int) getpid());
 
-    int rc = fork();
+    long diff;
+	struct timespec start, end;
+	long sum = 0;
+    double avg;
 
-    if (rc < 0) {
-        fprintf(stderr, "fork failed\n");
-        exit(1);
-    } else if (rc == 0) {
-        
-        printf("The child process (pid: %d)\n", (int) getpid());
-    } else {
-        
-        printf("The parent process (pid: %d)\n", (int) getpid());
+    /* measure monotonic time */
+    for (int i = 0; i < number_iter; i++) {
+        clock_gettime(CLOCK_MONOTONIC, &start);	/* mark start time */
+
+        write(fileno(stdout), NULL, 0);
+
+        clock_gettime(CLOCK_MONOTONIC, &end);	/* mark the end time */
+
+        diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+        sum += diff;
     }
+
+    avg = sum / (float) number_iter;
+
+	printf("\nAverage Time = %f nanoseconds\n", avg);
+
+	/* now re-do this and measure CPU time */
+    sum = 0;
+    for (int i = 0; i < number_iter; i++) {
+
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);	/* mark start time */
+
+	write(fileno(stdout), NULL, 0);
+
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);		/* mark the end time */
     
+    diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+    sum += diff;
+    }
+
+    avg = sum / (float) number_iter;
+
+    printf("\nAverage CPU Time = %f nanoseconds\n", avg);
+
     return 0;
 }
